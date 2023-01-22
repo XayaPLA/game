bb2ac8875af0c2b3847581703f85444398f607d9
EverydayFrog922 <szaboerikzalan@gmail.com>
2023-01-22 15:22:31 +0100

    na ez most a towerdefense, méghozzá az új Unity verzióban

---
+++ towerdef_brackeys_new/Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool DoMovement = true;
+    public float PanSpeed = 30f;
+    public float PanBoarderThickness = 10f;
+    public float ScrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY= 90f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            DoMovement = !DoMovement;
+        }
+        
+        if (!DoMovement) return;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - PanBoarderThickness)
+        {
+            transform.Translate(Vector3.forward * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= PanBoarderThickness)
+        {
+            transform.Translate(Vector3.back * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - PanBoarderThickness)
+        {
+            transform.Translate(Vector3.right * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= PanBoarderThickness)
+        {
+            transform.Translate(Vector3.left * PanSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * ScrollSpeed * Time.deltaTime;
+
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+    }
+}
---
+++ towerdef_brackeys_new/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 7f;
+    public float countdown = 10f;
+    private int WaveIndex = 0;
+    public Text countDownText;
+
+
+    void Update()
+    {
+        if (countdown <= 0) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        else if (countdown <= 5) {
+            countDownText.text = Mathf.Floor(countdown + 1).ToString();
+        }
+        else
+        {
+            countDownText.text = "Wave incoming!!";
+        }
+        countdown -= Time.deltaTime;
+        
+    }
+
+    IEnumerator SpawnWave()
+    {
+        WaveIndex++;
+        for (int i = 0; i < WaveIndex; i++)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(0.5f);
+        }
+        Debug.Log("Wave incoming!!");
+    }
+
+
+}
